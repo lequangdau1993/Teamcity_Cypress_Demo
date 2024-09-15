7f5345bc2d0b2ace77998f4514414b2b8f38e113
lequangdau1993 <43399313+lequangdau1993@users.noreply.github.com>
2024-09-15 16:46:09 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   'projectId': '4b7344',
-  e2e: {},
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    specPattern: [
+      'cypress/e2e/**/*.{coffee,feature,features,spec.js}',
+    ],
+    experimentalRunAllSpecs: true,
+    env: {
+      grepOmitFiltered: true,
+      grepFilterSpecs: true,
+    },
+  },
 }
