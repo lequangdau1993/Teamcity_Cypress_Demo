02b46f8950c5c0289cef8dcc91c97d654a8197ad
lequangdau1993 <43399313+lequangdau1993@users.noreply.github.com>
2024-09-21 21:43:24 +0700

    Update demo.spec.js

--- cypress/e2e/demo.spec.js
+++ cypress/e2e/demo.spec.js
@@ -3,7 +3,7 @@
 describe('example to-do app', () => {
     beforeEach(() => {
       cy.visit('https://www.dealerwebsite.co.uk/all-vehicles/')
-    })
+    });
   
     it('displays two todo items by default', () => {      
       cy.get('[title="Used"]').click();
