27ef71c54b0c42c45612e65b508e04ce45c21f73
lequangdau1993 <43399313+lequangdau1993@users.noreply.github.com>
2024-09-23 22:02:35 +0700

    Update demo.spec.js

--- cypress/e2e/demo.spec.js
+++ cypress/e2e/demo.spec.js
@@ -1,13 +1,34 @@
 /// <reference types="cypress" />
 
-describe('example to-do app', () => {
+describe('example test with cypress', () => {
     beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      cy.testStep("Access search listing page");
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
       cy.visit('https://www.dealerwebsite.co.uk/all-vehicles/')
-    });
+    })
   
-    it('displays two todo items by default', () => {      
-      cy.get('[title="Used"]').click();
+    it('Verify condition filter', () => {
+      cy.testStep("Click Used condition");
+      cy.get('.ndfe-search-filter--expanded .v-btn__content').contains('Used').click();
+
+      cy.testStep("Verify chip is display");
+      cy.get('[data-test="condition"]').find('.v-chip__content').contains('Used').should('be.visible');
     });
 
+    it('Verify Make filter', () => {
+      cy.testStep("Click Make filter");
+      cy.get('#ndfe-option_make').find('.v-select__slot').click();
+
+      cy.testStep("Select Make");
+      cy.get('.v-menu__content').contains('BMW').click();
+
+
+      cy.testStep("Verify chip is display");
+      cy.get('[data-test="manufacturer"]').find('.v-chip__content').contains('Audi').should('be.visible');
+    })
+  
   })
   
