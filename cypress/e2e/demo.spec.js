b0a31d06448680078a9a9e6d2f6d1d0024982bb6
lequangdau1993 <43399313+lequangdau1993@users.noreply.github.com>
2024-12-24 22:39:39 +0700

    Update demo.spec.js

--- cypress/e2e/demo.spec.js
+++ cypress/e2e/demo.spec.js
@@ -1,34 +1,29 @@
 /// <reference types="cypress" />
 
 describe('example test with cypress', () => {
-    beforeEach(() => {
-      // Cypress starts out with a blank slate for each test
-      cy.testStep("Access search listing page");
-      // so we must tell it to visit our website with the `cy.visit()` command.
-      // Since we want to visit the same URL at the start of all our tests,
-      // we include it in our beforeEach function so that it runs before each test
-      cy.visit('https://www.dealerwebsite.co.uk/all-vehicles/')
-    })
   
-    it('Verify condition filter', () => {
-      cy.testStep("Click Used condition");
-      cy.get('.ndfe-search-filter--expanded .v-btn__content').contains('Used').click();
+  it('Verify regression test running', { tags: '@regression' }, () => {
+    cy.testStep("regression test has been run");
+  });
 
-      cy.testStep("Verify chip is display");
-      cy.get('[data-test="condition"]').find('.v-chip__content').contains('Used').should('be.visible');
-    });
+  it('Verify critical test running', { tags: '@critical' }, () => {
+    cy.testStep("critical test has been run");
 
-    it('Verify Make filter', () => {
-      cy.testStep("Click Make filter");
-      cy.get('#ndfe-option_make').find('.v-select__slot').click();
-
-      cy.testStep("Select Make");
-      cy.get('.v-menu__content').contains('Audi').click();
-
-
-      cy.testStep("Verify chip is display");
-      cy.get('[data-test="manufacturer"]').find('.v-chip__content').contains('Audi').should('be.visible');
-    })
-  
   })
+
+  it('Verify regression & critical test running', { tags: ['@regression', '@critical'] }, () => {
+    cy.testStep("regression & critical test has been run");
+
+  })
+
+  it('Verify smoke test running', { tags: '@smoke' }, () => {
+    cy.testStep("smoke test has been run");
+
+  })
+
+  it('Verify normal test without tags running', () => {
+    cy.testStep("normal test without tags has been run");
+  })
+  
+})
   
